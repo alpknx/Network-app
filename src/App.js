42d99b8b47d62bb24dbdfe618c9783705737e406
Al <al@Als-MacBook-Air.local>
2022-03-21 12:49:55 +0300

    made the initial layout of the page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const  App = () =>  {
+	return (
+   <div className='app_wrapper'>
+		<div className='app_header_content'>
+			<Header/>
+		</div>
+		<div className='app_layout'>
+			<div className='side_bar'>
+				<SideBar/>
+			</div>
+			<div className="app_content">
+				<Content/>
+			</div>
+		</div>
+   </div>
+	);
+}
+
+const Header = () => {
+	return (
+		<div className='app_header_wrapper'>
+			Header
+		</div>
+	)
+}
+
+const SideBar = () => {
+	return (
+		<div className='side_bar_inner'>
+			Sidebar
+		</div>
+	)
+}
+
+const Content = () => {
+	return (
+		<div className='app_content_inner'>
+			Content
+		</div>
+	)
 }
 
 export default App;
