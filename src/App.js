d51bc6d24c2c3696fb30837947aae458c4592adb
Al <al@Als-MacBook-Air.local>
2022-03-21 19:43:01 +0300

    add routing and initial layouts for pages

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import SideBar from './components/SideBar/SideBar';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Users from './../src/components/Users/Users';
+import News from './../src/components/News/News';
+import Music from './../src/components/Music/Music';
+import Settings from './../src/components/Settings/Settings';
 
 const  App = () =>  {
 	return (
-		<div className='app_wrapper'>
-			<div className='app_header_content'>
-				<Header/>
-			</div>
-			<div className='app_layout'>
-				<div className='side_bar'>
-					<SideBar/>
+		<BrowserRouter>
+			<div className='app_wrapper'>
+				<div className='app_header_content'>
+					<Header/>
 				</div>
-				<div className="app_content">
-					{/* <Profile/> */}
-					<Dialogs/>
+				<div className='app_layout'>
+					<div className='side_bar'>
+						<SideBar/>
+					</div>
+					<div className="app_content">
+						<Routes>
+							<Route path="/profile" element={<Profile/>} />
+							<Route path="/dialogs" element={<Dialogs/>} />
+							<Route path="/users" element={<Users/>} />
+							<Route path="/news" element={<News/>} />
+							<Route path="/music" element={<Music/>} />
+							<Route path="/settings" element={<Settings/>} />
+						</Routes>
+					</div>
 				</div>
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 }
 
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,46 +6,46 @@ import musicIcon from './../assets/img/sidebar/music-icon.png';
 import settingsIcon from './../assets/img/sidebar/settings-icon.png';
 import cl from './SideBar.module.css';
 
-const SideBar = () => {
+const SideBar = (props) => {
 	return (
 		<div className={cl.side_bar_inner}>
 			<nav className={cl.side_bar_nav}>
 				<ol className={cl.side_bar_ol}>
 					<li className={cl.side_bar_item}>
-						<li className={cl.left_menu_icon}>
+						<div className={cl.left_menu_icon}>
 							<img src={profileIcon}/>
-						</li>
-						<span className={cl.left_label}>Profile</span>
+						</div>
+						<a href="/profile"><span className={cl.left_label}>Profile</span></a>
 					</li>
 					<li className={cl.side_bar_item}>
 						<div className={cl.left_menu_icon}>
 							<img src={messagesIcon}/>
 						</div>
-						<span className={cl.left_label}>Messages</span>
+						<a href="/dialogs"><span className={cl.left_label}>Messages</span></a>
 					</li>
 					<li className={cl.side_bar_item}>
 						<div className={cl.left_menu_icon}>
 							<img src={usersIcon}/>
 						</div>
-						<span className={cl.left_label}>Users</span>
+						<a href="/users"><span className={cl.left_label}>Users</span></a>
 					</li>
 					<li className={cl.side_bar_item}>
 						<div className={cl.left_menu_icon}>
 							<img src={newsIcon}/>
 						</div>
-						<span className={cl.left_label}>News</span>
+						<a href="/news"><span className={cl.left_label}>News</span></a>
 					</li>
 					<li className={cl.side_bar_item}>
 						<div className={cl.left_menu_icon}>
 							<img src={musicIcon}/>
 						</div>
-						<span className={cl.left_label}>Music</span>
+						<a href="/music"><span className={cl.left_label}>Music</span></a>
 					</li>
 					<li className={cl.side_bar_item}>
 						<div className={cl.left_menu_icon}>
 							<img src={settingsIcon}/>
 						</div>
-						<span className={cl.left_label}>Settings</span>
+						<a href="/settings"><span className={cl.left_label}>Settings</span></a>
 					</li>
 				</ol>
 			</nav>
