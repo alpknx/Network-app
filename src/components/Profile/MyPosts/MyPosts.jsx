685128ab366a5e6d10619c8a6dc40ad5de115c18
Al <al@Als-MacBook-Air.local>
2022-03-21 17:15:27 +0300

    selected post in a separate component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,24 +1,13 @@
 import cl from './MyPosts.module.css';
 import profileImg from './../../assets/img/profile-img.png'
+import Post from './Post/Post';
 
 const MyPosts = () => {
 	return (
 		<div className={cl.posts_list}>
 			<h1> My Posts </h1>
-			<div className={cl.post_item}>
-				<div className={cl.img_profile}>
-						<img src={profileImg}/>
-					</div>
-				<div className={cl.post_and_like}>
-					<div className={cl.post_text}>
-						new post
-					</div>
-					<div className={cl.post_likes}>
-						likes : 10 
-					</div>
-				</div>
+			<Post/>
 			</div>
-		</div>
 	);
 }
 
