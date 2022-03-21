5c905079c2af65c92a5baac3415b866f13b11dcc
Al <al@Als-MacBook-Air.local>
2022-03-22 00:27:31 +0300

    put the data into an array

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,6 +3,22 @@ import DialogItem from './DialogItem/DialogITem';
 import MessageItem from './MessageItem/MessageItem';
 
 const Dialogs = (props) => {
+
+	let dialogsData = [
+		{id: 1, name: 'Alexander'},
+		{id: 2, name: 'Sergey'},
+		{id: 3, name: 'Sveta'},
+		{id: 4, name: 'Julia'},
+		{id: 5, name: 'Stepan'}
+	];
+
+	let messagesData = [
+		{id: 1, message: 'Hello'},
+		{id: 2, message: 'Check this linnk'},
+		{id: 3, message: 'Once upon a time'},
+		{id: 4, message: 'I missed about you'},
+		{id: 5, message: 'Once upon a time'}
+	];
 	return (
 		<div className={cl.dialogs_page}>
 			<div className={cl.header_page}>
@@ -10,18 +26,18 @@ const Dialogs = (props) => {
 			</div>
 			<div className={cl.message_section}>
 				<ul className={cl.dialogs_list}>
-					<DialogItem name="Alexander" id="1"/>
-					<DialogItem name="Sergey" id="2"/>
-					<DialogItem name="Sveta" id="3"/>
-					<DialogItem name="Julia" id="4"/>
-					<DialogItem name="Stepan" id="5"/>
+					<DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+					<DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+					<DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+					<DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+					<DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
 				</ul>
 				<ul className={cl.messages_list}>
-				<MessageItem message="Hello"/>
-				<MessageItem message="Check this linnk"/>
-				<MessageItem message="Once upon a time"/>
-				<MessageItem message="I missed about you"/>
-				<MessageItem message="Once upon a time"/>
+				<MessageItem message={messagesData[0].message}/>
+				<MessageItem message={messagesData[1].message}/>
+				<MessageItem message={messagesData[2].message}/>
+				<MessageItem message={messagesData[3].message}/>
+				<MessageItem message={messagesData[4].message}/>
 				</ul>
 			</div>
 		</div>
