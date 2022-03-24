74f54b75c6b0eb64ef6f9157d08e06293338c8fa
Al <al@Als-MacBook-Air.local>
2022-03-24 10:22:37 +0300

    remove side effect in onClick button

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,19 +3,24 @@ import User from './User/User';
 import cl from './Users.module.css';
 
 const Users = (props) => {
-	if (props.users.length === 0) {
 
-		axios.get("https://social-network.samuraijs.com/api/1.0/users")
-			.then(response => {
-				props.setUsers(response.data.items);
-			})
-
+	let getUsers = () => {
 		
+		if (props.users.length === 0) {
+	
+			axios.get("https://social-network.samuraijs.com/api/1.0/users")
+				.then(response => {
+					props.setUsers(response.data.items);
+				})
+	
+			
+		}
 	}
 
 	debugger;
 	return (
 		<div className={cl.users}>
+			<button onClick={getUsers}>Get Users</button>
 			<User users={props.users} follow={props.follow} unfollow={props.unfollow}/>
 		</div>
 	);
