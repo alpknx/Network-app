03361150f3e263a1928305dc63e5d21cd3356f8b
Al <al@Als-MacBook-Air.local>
2022-04-20 12:45:05 +0300

    add check for length state

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -41,8 +41,13 @@ const usersReducer = (state = initialState , action) => {
 		}
 
 		case SET_USERS: {
-         return { ...state,
-						users: [ ...action.users ]}
+			if(state.users.length > 0) {
+				return state
+			}
+			return { ...state,
+				users:[ ...state.users, ...action.users ],
+				
+			  }
       }
 		case SET_CURRENT_PAGE: {
          return { ...state,
