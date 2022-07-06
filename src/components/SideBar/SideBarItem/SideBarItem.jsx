50d548393c46be44e4a40311e0dc61ed4376e841
alpknx <alpknx@gmail.com>
2022-07-06 13:10:12 +0300

    fix(project): add alt

--- src/components/SideBar/SideBarItem/SideBarItem.jsx
+++ src/components/SideBar/SideBarItem/SideBarItem.jsx
@@ -5,7 +5,7 @@ const SideBarItem = (props) => {
   return (
     <li className={cl.side_bar_item}>
       <div className={cl.left_menu_icon}>
-        <img src={props.icon} />
+        <img src={props.icon} alt='icon' />
       </div>
       <NavLink to={props.link} className={({ isActive }) => (isActive ? `${cl.activeLink}` : `${cl.left_label}`)}>
         {props.SideBarItem}
