f2d0f2d919306dc4e82de5b3b5f311941115d2c8
alpknx <alpknx@gmail.com>
2022-07-06 13:34:28 +0300

    fix(project): remove tests

--- src/Redux/profile.reducer.test.js
+++ src/Redux/profile.reducer.test.js
@@ -19,36 +19,3 @@ test('length of posts should be incremented', () => {
   // 3. expectation
   expect(newState.postsData.length).toEqual(5);
 });
-
-// test('message of new post should be correct', () => {
-//   // 1. test data
-//   const action = onAddPostAC('it-kamasutra.com');
-
-//   // 2. action
-//   const newState = profileReducer(state, action);
-
-//   // 3. expectation
-//   expect(newState.postsData[4].message).toBeValid('it-kamasutra.com');
-// });
-
-// test('after deleting length of messages should be decrement', () => {
-//   // 1. test data
-//   const action = deletePost(1);
-
-//   // 2. action
-//   const newState = profileReducer(state, action);
-
-//   // 3. expectation
-//   expect(newState.postsData.length).toEqual(3);
-// });
-
-// test("after deleting length shouldn't be decrement if id is incorrect", () => {
-//   // 1. test data
-//   const action = deletePost(1000);
-
-//   // 2. action
-//   const newState = profileReducer(state, action);
-
-//   // 3. expectation
-//   expect(newState.postsData.length).toEqual(4);
-// });
